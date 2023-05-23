758cd370a42f29c168e50a5006575186a4d9382c
Laura Leon <gruponumero2sisinf2@outlook.com>
2023-05-23 19:12:57 -0400

    Envio

--- src/GitExpertApp.jsx
+++ src/GitExpertApp.jsx
@@ -1,14 +1,18 @@
 import { useState } from "react"
 import { AddCategory } from "./components/AddCategory";
+import { GifGrid } from "./components/GifGrid";
 
 export const GitExpertApp = () => {
 
-const [categories, setCategories] = useState(['One Punch','Dragon ball']) ;
+const [categories, setCategories] = useState(['One Punch']) ;
 console.log(categories);
 
-const onAddCategory=()=>{
-console.log('valorant');
-     setCategories([...categories,'hola'])//hacemos una copia de lo q ya teniam,os  ...categories
+const onAddCategory=(newCategory)=>{
+  //validacion de los nombres 
+  if(categories.includes(newCategory))return;
+//console.log(newCategory);
+//categories.push(newCategory)
+  setCategories([newCategory,...categories])//hacemos una copia de lo q ya teniam,os  ...categories
 
   //setCategories(cat=>[...cat,'valor'])//esto tiene una forma de desestructurar creamois un callback
 
@@ -20,15 +24,19 @@ console.log('valorant');
      <h1>GifExpertApp</h1>
 
      {/* input */}
-    <AddCategory/>
+    <AddCategory 
+    // setCategories={setCategories}
+    onNewCategory={(value)=>onAddCategory(value)}
+    />
      {/* listado de gif */}
-     <button onClick={onAddCategory}>Agregar</button>
-    <ol>
-      {categories.map(category=>{
-        return <li key={category}>{category}</li>
-      })}
+     {/* <button onClick={onAddCategory}>Agregar</button> */}
+      {
+      categories.map(category=>(
+        <GifGrid key={category}
+        category={category}/>
+           )     )
+}
       {/* //barrer cada uno de los elementos */}
-    </ol>
      </>
   )
 }
--- src/components/AddCategory.jsx
+++ src/components/AddCategory.jsx
@@ -1,29 +1,33 @@
 import { useState } from "react"
 
 
-export const AddCategory = ({setCategories}) => {
-const [inputValue, setInputValue] = useState('One Puch')
-const onInputChange=({target})=>{//desestructuramos y ya no mandamos el event cuando solo tomamos el valor de target
-    //setInput('hola')
-    //console.log(target.value);
-    setInputValue(target.value)
-}
-const onSubmit=(event)=>{
-    event.preventDefault();//cuando aga enter no se recargue  la pagina 
-    console.log(inputValue);
-    //setCategories(...inputValue,)
+export const AddCategory = ({onNewCategory}) => {
+const [inputValue,SetInputValue]=useState('');
+
+
+const onInputChange=({target})=>{
+  //console.log(target.value);
+  SetInputValue(target.value)
 
 }
-
+const onSubmmit=(event)=>{
+  event.preventDefault();
+  //console.log(inputValue);
+  if (inputValue.trim().length<=1)return;
+//setCategories(categories=>[inputValue,...categories])
+onNewCategory(inputValue.trim());
+SetInputValue('')
+}
   return (
-    <form onSubmit={(event)=> onSubmit(event)}>
-        <input
-            type="text"
-            placeholder="Buscar gifs"
-            value={inputValue}
-            onChange={onInputChange}
-        />
+  
+    <form onSubmit={onSubmmit}>
+    <input 
+    type="text" 
+    placeholder="Buscar gifs"
+    value={inputValue}
+    onChange={onInputChange}
+    />
     </form>
-
   )
 }
+
